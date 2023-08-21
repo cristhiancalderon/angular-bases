4a3026b864b2412a482c449c03049d9e704f7ebf
Cristhian Calderon <cristhian.calderon.mackay@gmail.com>
2023-08-21 13:26:13 +0200

    update name module heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from './heroes/heroes.module.ts/heroes.modele';
+import { HeroesModule } from './heroes/heroes.module.ts/heroes.module';
 
 
 @NgModule({
